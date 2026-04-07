b154944a8c6fde9e48037530054baca44d2888c8
baanshabriri <abhinandandas77@gmail.com>
2026-04-07 12:24:05 +0530

    process image changes

--- core/models.py
+++ core/models.py
@@ -45,6 +45,7 @@ class CompanyOverview(models.Model):
     heading = models.CharField(max_length=200, default="COMPANY OVERVIEW")
     body = models.TextField()
     image = models.ImageField(upload_to="company_overview/", blank=True)
+    process_image = models.ImageField(upload_to="company_overview/", blank=True)
 
     def __str__(self):
         return "Company Overview"
